8b01372fa75ba22de9ea6694d07389c0301fcf55
shouldsee <shouldsee@github.com>
2020-03-18 15:11:20 +0800

    random_seq_const -> random_seq-const

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 BIN=${1:-python3}
 rm -rf /tmp/test_remote/ -rf
+ARGS=( TOPLEVEL:run_and_backup /tmp/test_remote/root 1 2 /tmp/test_remote/root.backup )
+$BIN -m spiper run spiper_mock_flow@file://$PWD ${ARGS[@]}
+exit 0
 $BIN -m spiper run \
   spiper_mock_flow@file://$PWD \
   TOPLEVEL:run_and_backup \
